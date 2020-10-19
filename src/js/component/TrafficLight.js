287f9dd2cc3992370311b925e6190f853c0f2703
MLopez8282 <saltairpix@gmail.com>
2020-10-19 17:55:43 +0000

    basics

---
+++ src/js/component/TrafficLight.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export const TrafficLight = () => {
+	//const[color, setColor] = useState("");
+	// const changeLight = (e) = {
+	//   if(e.target.classList = setColor
+
+	//}
+	return (
+		<>
+			<div className="top" />
+			<div className="rectangle">
+				<div className="red-circle" />
+				<div className="yellow-circle" />
+				<div className="green-circle" />
+			</div>
+		</>
+	);
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TrafficLight } from "./TrafficLight";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,18 +8,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<TrafficLight />
 		</div>
 	);
 }
