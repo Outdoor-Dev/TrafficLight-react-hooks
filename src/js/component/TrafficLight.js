808aaaa116500a522de27271015b88f48f86f67d
MLopez8282 <saltairpix@gmail.com>
2020-10-20 01:48:35 +0000

    icons

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,18 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const TrafficLight = () => {
-	//const[color, setColor] = useState("");
-	// const changeLight = (e) = {
-	//   if(e.target.classList = setColor
+	const [color, setColor] = useState("green");
+	useEffect(
+		() => {
+			setTimeout(() => {
+				if (color == "green") {
+					setColor("yellow");
+				} else if (color == "yellow") {
+					setColor("red");
+				} else if (color == "red") {
+					setColor("green");
+				}
+			}, 2000);
+		},
+		[color]
+	);
 
-	//}
 	return (
 		<>
-			<div className="top" />
 			<div className="rectangle">
-				<div className="red-circle" />
-				<div className="yellow-circle" />
-				<div className="green-circle" />
+				<div
+					className={
+						color == "red" ? "red-circle selected" : "red-circle"
+					}>
+					<i className="icon-hand far fa-hand-paper" />
+				</div>
+				<div
+					className={
+						color == "yellow"
+							? "yellow-circle selected"
+							: "yellow-circle"
+					}>
+					<img
+						className="turtle"
+						src="https://img.icons8.com/ios/50/000000/turtle.png"
+					/>
+				</div>
+
+				<div
+					className={
+						color == "green"
+							? "green-circle selected"
+							: "green-circle"
+					}>
+					<i className="icon-walking fas fa-walking" />
+				</div>
 			</div>
 		</>
 	);
